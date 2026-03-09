38164281db13365faa3ff4bb3829376a4682d868
Atarox <80927839+Atarox228@users.noreply.github.com>
2026-03-09 20:20:38 -0300

    commit para deploy en github

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/politica-mellao/',   // ← el nombre exacto de tu repositorio en GitHub
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
